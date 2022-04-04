7a73d0ced6e92befb4a6cbdaddcd1f2396eb5e5f
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-04-04 14:07:53 +0300

    Feat: add entities in acc.entity
    add AccAccountActivity
    add AccMoneyTransfer
    add AccAccountActivityType
    add AccTransferType

---
+++ src/main/java/com/cbarkinozer/onlinebankingrestapi/app/acc/entity/AccAccountActivity.java
@@ -0,0 +1,39 @@
+package com.cbarkinozer.onlinebankingrestapi.app.acc.entity;
+
+import com.cbarkinozer.onlinebankingrestapi.app.acc.enums.AccAccountActivityType;
+import com.cbarkinozer.onlinebankingrestapi.app.gen.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name="ACC_ACCOUNT_ACTIVITY")
+public class AccAccountActivity extends BaseEntity {
+
+    @Id
+    @SequenceGenerator(name="AccAccountActivity",sequenceName = "ACC_ACCOUNT_ACTIVITY_ID_SEQ")
+    @GeneratedValue(generator = "AccAccountActivity")
+    private Long id;
+
+    @Column(name="ID_ACC_ACCOUNT",length = 100,nullable = false)
+    private Long accountId;
+
+    @Column(name="AMOUNT",precision=19,scale=2,nullable = false)
+    private BigDecimal amount;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="TRANSACTION_DATE",nullable = false)
+    private LocalDate transactionDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="ACCOUNT_ACTIVITY_TYPE",length = 30,nullable = false)
+    private AccAccountActivityType accountActivityType;
+
+    @Column(name="CURRENT_BALANCE",precision = 19 ,scale =2 ,nullable = false)
+    private BigDecimal currentBalance;
+}
---
+++ src/main/java/com/cbarkinozer/onlinebankingrestapi/app/acc/entity/AccMoneyTransfer.java
@@ -0,0 +1,42 @@
+package com.cbarkinozer.onlinebankingrestapi.app.acc.entity;
+
+import com.cbarkinozer.onlinebankingrestapi.app.acc.enums.AccTransferType;
+import com.cbarkinozer.onlinebankingrestapi.app.gen.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name="ACC_MONEY_TRANSFER")
+public class AccMoneyTransfer extends BaseEntity {
+
+    @Id
+    @SequenceGenerator(name="AccMoneyTransfer",sequenceName = "ACC_MONEY_TRANSFER_ID_SEQ")
+    @GeneratedValue(generator = "AccMoneyTransfer")
+    private Long id;
+
+    @Column(name="ID_ACC_ACCOUNT_FROM",length = 100,nullable = false)
+    private Long accountIdFrom;
+
+    @Column(name="ID_ACC_ACCOUNT_TO",length = 100,nullable = false)
+    private Long accountIdTo;
+
+    @Column(name ="AMOUNT" ,precision = 19,scale = 2,nullable = false)
+    private BigDecimal amount;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "transferDate",nullable = false)
+    private LocalDate transferDate;
+
+    @Column(name="DESCRIPTION",length=280,nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="TRANSFER_TYPE",length=30,nullable = false)
+    private AccTransferType transferType;
+}
